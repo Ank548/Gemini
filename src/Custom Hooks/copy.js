e96f2166597b85eb9ff9622a2a54d46c740ff8b7
Sanket <sanketj@trentiums.com>
2024-07-05 11:32:48 +0530

    fixed code inside response with marked

--- src/Custom Hooks/copy.js
+++ src/Custom Hooks/copy.js
@@ -1,21 +1,21 @@
 import { assets } from "../assets/assets";
 
 function Copy(text, e) {
-    let copyElement;
-    if (e.target.closest('.copy')) {
-        copyElement = e.target.closest('.copy')
-    }
-    else {
-        const children = Array.from(e.target.closest('div').children);
-        copyElement = children.find(child => child.classList.contains('copy'));
-    }
+    const copyDiv = e.target.closest('div');
+    const copyElement = copyDiv.querySelector('.copy')
 
-    navigator.clipboard.writeText(text);
-
-    copyElement.src = assets.check_icon
-    setTimeout(() => {
-        copyElement.src = assets.copy_icon
-    }, 2000);
+    if (copyElement) {
+        navigator.clipboard.writeText(text)
+            .then(() => {
+                copyElement.src = assets.check_icon;
+                setTimeout(() => {
+                    copyElement.src = assets.copy_icon;
+                }, 2000);
+            })
+            .catch((error) => {
+                console.error('Failed to copy:', error);
+            });
+    }
 }
 
 export default Copy
